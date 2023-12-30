3cec19bffec4731e1c6229f29afdeee5a782dbde
Kyle Green <kyle.green999@gmail.com>
2023-12-30 00:27:40 +0000

    Project setup

---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+use std::io::{Write, Read};
+
+use byteorder::{WriteBytesExt, ReadBytesExt};
+use err_derive::Error;
+
+pub(crate) type ByteOrder = byteorder::BigEndian;
+
+pub mod se;
+
+pub(crate) mod data_ids {
+    pub const NULL_ID: u8 = 0;
+    pub const BOOL_ID: u8 = 1;
+    pub const I8_ID: u8 = 2;
+    pub const I16_ID: u8 = 3;
+    pub const I32_ID: u8 = 4;
+    pub const I64_ID: u8 = 5;
+    pub const U8_ID: u8 = 6;
+    pub const U16_ID: u8 = 7;
+    pub const U32_ID: u8 = 8;
+    pub const U64_ID: u8 = 9;
+    pub const F32_ID: u8 = 10;
+    pub const F64_ID: u8 = 11;
+    pub const CHAR_ID: u8 = 12;
+    pub const STR_ID: u8 = 13;
+    pub const BYTES_ID: u8 = 14;
+    pub const SEQ_ID: u8 = 15;
+    pub const TUPLE_ID: u8 = 16;
+    pub const UNIT_VARIANT_ID: u8 = 17;
+    pub const NEWTYPE_VARIANT_ID: u8 = 18;
+    pub const TUPLE_VARIANT_ID: u8 = 19;
+    pub const STRUCT_VARIANT_ID: u8 = 20;
+    pub const TUPLE_STRUCT_ID: u8 = 21;
+    pub const MAP_ID: u8 = 22;
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(display = "IO error: {}", _0)]
+    IoError(#[source] std::io::Error),
+    #[error(display = "From utf8 error: {}", _0)]
+    FromUtf8Error(#[source] std::string::FromUtf8Error),
+    #[error(display = "'{}' is not a valid compression format", _0)]
+    InvalidCompression(u8),
+    #[error(display = "{}", _0)]
+    Custom(String),
+    #[error(display = "Lengths are required for the sbif format")]
+    LengthRequired,
+}
+
+impl serde::ser::Error for Error {
+    fn custom<T>(msg: T) -> Self where T:std::fmt::Display {
+        Self::Custom(msg.to_string())
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Compression {
+    None,
+    Deflate(u32),
+    Gzip(u32),
+    Zlib(u32)
+}
+
+impl Default for Compression {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
+pub(crate) struct FileHeader {
+    pub(crate) compression: Compression,
+    pub(crate) version: u8,
+    pub(crate) header_name: String,
+}
+
+impl FileHeader {
+    pub fn new(compression: Compression) -> Self {
+        Self {
+            compression,
+            version: 1,
+            header_name: String::from("SBIF"),
+        }
+    }
+
+    pub fn to_writer<W: Write>(&self, writer: &mut W) -> Result<(), Error> {
+        let name_bytes = self.header_name.as_bytes();
+        writer.write_u16::<ByteOrder>(name_bytes.len() as u16).map_err(Error::IoError)?;
+        writer.write(name_bytes).map_err(Error::IoError)?;
+        writer.write_u8(self.version).map_err(Error::IoError)?;
+
+        match self.compression {
+            Compression::None => writer.write_u8(0).map_err(Error::IoError)?,
+            Compression::Deflate(v) => {
+                writer.write_u8(1).map_err(Error::IoError)?;
+                writer.write_u32::<ByteOrder>(v).map_err(Error::IoError)?;
+            },
+            Compression::Gzip(v) => {
+                writer.write_u8(2).map_err(Error::IoError)?;
+                writer.write_u32::<ByteOrder>(v).map_err(Error::IoError)?;
+            },
+            Compression::Zlib(v) => {
+                writer.write_u8(3).map_err(Error::IoError)?;
+                writer.write_u32::<ByteOrder>(v).map_err(Error::IoError)?;
+            },
+        };
+
+        Ok(())
+    }
+
+    pub fn from_reader<R: Read>(reader: &mut R) -> Result<Self, Error> {
+        let header_name = {
+            let name_length = reader.read_u16::<ByteOrder>().map_err(Error::IoError)? as usize;
+            let mut buffer = vec![0_u8; name_length];
+            reader.read_exact(&mut buffer).map_err(Error::IoError)?;
+            String::from_utf8(buffer).map_err(Error::FromUtf8Error)?
+        };
+
+        let version = reader.read_u8().map_err(Error::IoError)?;
+        let compression = match reader.read_u8().map_err(Error::IoError)? {
+            0 => Compression::None,
+            1 => Compression::Deflate(reader.read_u32::<ByteOrder>().map_err(Error::IoError)?),
+            2 => Compression::Gzip(reader.read_u32::<ByteOrder>().map_err(Error::IoError)?),
+            3 => Compression::Zlib(reader.read_u32::<ByteOrder>().map_err(Error::IoError)?),
+            v => return Err(Error::InvalidCompression(v)),
+        };
+
+        Ok(Self { compression, version, header_name  })
+    }
+}
---
+++ src/se.rs
@@ -0,0 +1,542 @@
+use std::io::Write;
+
+use byteorder::WriteBytesExt;
+use flate2::write::{DeflateEncoder, GzEncoder, ZlibEncoder};
+
+use crate::{Compression, Error, FileHeader, ByteOrder};
+
+pub fn to_bytes<T: serde::Serialize>(value: &T) -> Result<Vec<u8>, Error> {
+    let mut buffer = Vec::new();
+    let mut serializer = Serializer::new(&mut buffer, Compression::default())?;
+    value.serialize(&mut serializer)?;
+    drop(serializer);
+
+    Ok(buffer)
+}
+
+enum Writer<'a> {
+    Borrowed(&'a mut dyn Write),
+    Owned(Box<dyn Write + 'a>),
+}
+
+impl<'a> Write for Writer<'a> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            Writer::Borrowed(w) => w.write(buf),
+            Writer::Owned(w) => w.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self {
+            Writer::Borrowed(w) => w.flush(),
+            Writer::Owned(w) => w.flush(),
+        }
+    }
+}
+
+pub struct Serializer<'a>(Writer<'a>);
+
+impl<'a> Serializer<'a> {
+    pub fn new<W: Write>(writer: &'a mut W, compression: Compression) -> Result<Self, Error> {
+        FileHeader::new(compression).to_writer(writer)?;
+        let writer: Writer<'a> = match compression {
+            Compression::None => Writer::Borrowed(writer),
+            Compression::Deflate(v) => Writer::Owned(Box::new(DeflateEncoder::new(writer, flate2::Compression::new(v)))),
+            Compression::Gzip(v) => Writer::Owned(Box::new(GzEncoder::new(writer, flate2::Compression::new(v)))),
+            Compression::Zlib(v) => Writer::Owned(Box::new(ZlibEncoder::new(writer, flate2::Compression::new(v)))),
+        };
+
+        Ok(Self(writer))
+    }
+}
+
+impl<'a, 'b> serde::ser::Serializer for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::BOOL_ID).map_err(Error::IoError)?;
+        self.0.write_u8(if v { 1 } else { 0 }).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::I8_ID).map_err(Error::IoError)?;
+        self.0.write_i8(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::I16_ID).map_err(Error::IoError)?;
+        self.0.write_i16::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::I32_ID).map_err(Error::IoError)?;
+        self.0.write_i32::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::I64_ID).map_err(Error::IoError)?;
+        self.0.write_i64::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::U8_ID).map_err(Error::IoError)?;
+        self.0.write_u8(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::U16_ID).map_err(Error::IoError)?;
+        self.0.write_u16::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::U32_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::U64_ID).map_err(Error::IoError)?;
+        self.0.write_u64::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::F32_ID).map_err(Error::IoError)?;
+        self.0.write_f32::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::F64_ID).map_err(Error::IoError)?;
+        self.0.write_f64::<ByteOrder>(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
+        let utf8_bytes = v.to_string().into_bytes();
+        self.0.write_u8(crate::data_ids::CHAR_ID).map_err(Error::IoError)?;
+        self.0.write_u8(utf8_bytes.len() as u8).map_err(Error::IoError)?;
+        self.0.write(&utf8_bytes).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.as_bytes();
+        self.0.write_u8(crate::data_ids::STR_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(bytes.len() as u32).map_err(Error::IoError)?;
+        self.0.write(bytes).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::BYTES_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(v.len() as u32).map_err(Error::IoError)?;
+        self.0.write(v).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::NULL_ID).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_some<T: ?Sized + serde::Serialize>(self, value: &T) -> Result<Self::Ok, Self::Error> {
+        value.serialize(self)
+    }
+
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        self.serialize_none()
+    }
+
+    fn serialize_unit_struct(self, _: &'static str) -> Result<Self::Ok, Self::Error> {
+        self.serialize_none()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::UNIT_VARIANT_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(variant_index).map_err(Error::IoError)?;
+        Ok(())
+    }
+
+    fn serialize_newtype_struct<T: ?Sized + serde::Serialize>(
+        self,
+        _name: &'static str,
+        value: &T,
+    ) -> Result<Self::Ok, Self::Error> {
+        value.serialize(self)
+    }
+
+    fn serialize_newtype_variant<T: ?Sized + serde::Serialize>(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        value: &T,
+    ) -> Result<Self::Ok, Self::Error> {
+        self.0.write_u8(crate::data_ids::NEWTYPE_VARIANT_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(variant_index).map_err(Error::IoError)?;
+        value.serialize(self)
+    }
+
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        let len = len.ok_or(Error::LengthRequired)?;
+        self.0.write_u8(crate::data_ids::SEQ_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        self.0.write_u8(crate::data_ids::TUPLE_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleStruct, Self::Error> {
+        self.0.write_u8(crate::data_ids::TUPLE_STRUCT_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleVariant, Self::Error> {
+        self.0.write_u8(crate::data_ids::TUPLE_VARIANT_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(variant_index).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        let len = len.ok_or(Error::LengthRequired)?;
+        self.0.write_u8(crate::data_ids::MAP_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+
+    fn serialize_struct(
+        self,
+        _name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeStruct, Self::Error> {
+        self.0.write_u8(crate::data_ids::MAP_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeStructVariant, Self::Error> {
+        self.0.write_u8(crate::data_ids::STRUCT_VARIANT_ID).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(variant_index).map_err(Error::IoError)?;
+        self.0.write_u32::<ByteOrder>(len as u32).map_err(Error::IoError)?;
+        Ok(self)
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeSeq for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized + serde::Serialize>(&mut self, value: &T) -> Result<(), Self::Error> {
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeTuple for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized + serde::Serialize>(&mut self, value: &T) -> Result<(), Self::Error> {
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeTupleStruct for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized + serde::Serialize>(&mut self, value: &T) -> Result<(), Self::Error> {
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeTupleVariant for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized + serde::Serialize>(&mut self, value: &T) -> Result<(), Self::Error> {
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeMap for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T: ?Sized + serde::Serialize>(&mut self, key: &T) -> Result<(), Self::Error> {
+        key.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn serialize_value<T: ?Sized + serde::Serialize>(&mut self, value: &T) -> Result<(), Self::Error> {
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeStruct for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized + serde::Serialize>(
+        &mut self,
+        key: &'static str,
+        value: &T,
+    ) -> Result<(), Self::Error> {
+        let key_bytes = key.as_bytes();
+        self.0.write_u32::<ByteOrder>(key_bytes.len() as u32).map_err(Error::IoError)?;
+        self.0.write(key_bytes).map_err(Error::IoError)?;
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<'a, 'b> serde::ser::SerializeStructVariant for &'a mut Serializer<'b> {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized + serde::Serialize>(
+        &mut self,
+        key: &'static str,
+        value: &T,
+    ) -> Result<(), Self::Error> {
+        let key_bytes = key.as_bytes();
+        self.0.write_u32::<ByteOrder>(key_bytes.len() as u32).map_err(Error::IoError)?;
+        self.0.write(key_bytes).map_err(Error::IoError)?;
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use serde::Serialize;
+
+    use crate::data_ids;
+
+    use super::*;
+
+    fn no_compression_serialization_test<T: serde::Serialize>(value: &T) -> Vec<u8> {
+        let serialized = to_bytes(value).unwrap();
+        assert!(serialized.len() >= 8);
+        assert_eq!(&serialized[0..8], &[0, 4, 83, 66, 73, 70, 1, 0]);
+
+        (&serialized[8..]).to_vec()
+    }
+
+    #[test]
+    fn test_bool_serialization() {
+        let test = no_compression_serialization_test(&true);
+        assert_eq!(test.as_slice(), &[data_ids::BOOL_ID, 1]);
+        let test = no_compression_serialization_test(&false);
+        assert_eq!(test.as_slice(), &[data_ids::BOOL_ID, 0]);
+    }
+
+    #[test]
+    fn test_integer_serialization() {
+        let test = no_compression_serialization_test(&1_u8);
+        assert_eq!(test.as_slice(), &[data_ids::U8_ID, 1]);
+        let test = no_compression_serialization_test(&1_u16);
+        assert_eq!(test.as_slice(), &[data_ids::U16_ID, 0, 1]);
+        let test = no_compression_serialization_test(&1_u32);
+        assert_eq!(test.as_slice(), &[data_ids::U32_ID, 0, 0, 0, 1]);
+        let test = no_compression_serialization_test(&1_u64);
+        assert_eq!(test.as_slice(), &[data_ids::U64_ID, 0, 0, 0, 0, 0, 0, 0, 1]);
+
+        let test = no_compression_serialization_test(&1_i8);
+        assert_eq!(test.as_slice(), &[data_ids::I8_ID, 1]);
+        let test = no_compression_serialization_test(&1_i16);
+        assert_eq!(test.as_slice(), &[data_ids::I16_ID, 0, 1]);
+        let test = no_compression_serialization_test(&1_i32);
+        assert_eq!(test.as_slice(), &[data_ids::I32_ID, 0, 0, 0, 1]);
+        let test = no_compression_serialization_test(&1_i64);
+        assert_eq!(test.as_slice(), &[data_ids::I64_ID, 0, 0, 0, 0, 0, 0, 0, 1]);
+    }
+
+    #[test]
+    fn test_float_serialization() {
+        let test = no_compression_serialization_test(&1_f32);
+        assert_eq!(test.as_slice(), &[data_ids::F32_ID, 63, 128, 0, 0]);
+        let test = no_compression_serialization_test(&1_f64);
+        assert_eq!(test.as_slice(), &[data_ids::F64_ID, 63, 240, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_char_serialization() {
+        let test = no_compression_serialization_test(&'a');
+        assert_eq!(test.as_slice(), &[data_ids::CHAR_ID, 1, 97]);
+    }
+
+    #[test]
+    fn test_string_serialization() {
+        let test = no_compression_serialization_test(&"hello world");
+        assert_eq!(test.as_slice(), &[data_ids::STR_ID, 0, 0, 0, 11, 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]);
+    }
+
+    #[test]
+    fn test_null_serialization() {
+        #[derive(Serialize)]
+        struct UnitStruct;
+
+        let test = no_compression_serialization_test(&Option::<u32>::None);
+        assert_eq!(test.as_slice(), &[data_ids::NULL_ID]);
+        let test = no_compression_serialization_test(&());
+        assert_eq!(test.as_slice(), &[data_ids::NULL_ID]);
+        let test = no_compression_serialization_test(&UnitStruct);
+        assert_eq!(test.as_slice(), &[data_ids::NULL_ID]);
+    }
+
+    #[test]
+    fn test_enum_serialization() {
+        #[derive(Serialize)]
+        enum TestEnum {
+            Unit,
+            NewType(u8),
+            Tuple(u8, u8),
+            Struct { a: u8, b: u8 }
+        }
+
+        let test = no_compression_serialization_test(&TestEnum::Unit);
+        assert_eq!(test.as_slice(), &[data_ids::UNIT_VARIANT_ID, 0, 0, 0, 0]);
+        let test = no_compression_serialization_test(&TestEnum::NewType(1));
+        assert_eq!(test.as_slice(), &[data_ids::NEWTYPE_VARIANT_ID, 0, 0, 0, 1, data_ids::U8_ID, 1]);
+        let test = no_compression_serialization_test(&TestEnum::Tuple(1, 2));
+        assert_eq!(test.as_slice(), &[data_ids::TUPLE_VARIANT_ID, 0, 0, 0, 2, 0, 0, 0, 2, data_ids::U8_ID, 1, data_ids::U8_ID, 2]);
+        let test = no_compression_serialization_test(&TestEnum::Struct { a: 1, b: 2 });
+        assert_eq!(test.as_slice(), &[
+            data_ids::STRUCT_VARIANT_ID,
+            0, 0, 0, 3, // variant index
+            0, 0, 0, 2, // length
+            0, 0, 0, 1, 97, data_ids::U8_ID, 1, // a
+            0, 0, 0, 1, 98, data_ids::U8_ID, 2  // b
+        ]);
+    }
+
+    #[test]
+    fn test_newtype_struct_serialization() {
+        #[derive(Serialize)]
+        struct NewtypeTest(u8);
+
+        let test = no_compression_serialization_test(&NewtypeTest(1));
+        assert_eq!(test.as_slice(), &[data_ids::U8_ID, 1]);
+    }
+
+    #[test]
+    fn test_tuple_struct_serialization() {
+        #[derive(Serialize)]
+        struct TupleTest(u8, u8);
+
+        let test = no_compression_serialization_test(&TupleTest(1, 2));
+        assert_eq!(test.as_slice(), &[data_ids::TUPLE_STRUCT_ID, 0, 0, 0, 2, data_ids::U8_ID, 1, data_ids::U8_ID, 2]);
+    }
+
+    #[test]
+    fn test_struct_serialization() {
+        #[derive(Serialize)]
+        struct StructTest {
+            a: u8,
+            b: u8
+        }
+
+        let test = no_compression_serialization_test(&StructTest { a: 1, b: 2 });
+        assert_eq!(test.as_slice(), &[
+            data_ids::MAP_ID,
+            0, 0, 0, 2, // length
+            0, 0, 0, 1, 97, data_ids::U8_ID, 1, // a
+            0, 0, 0, 1, 98, data_ids::U8_ID, 2  // b
+        ]);
+    }
+
+    #[test]
+    fn test_map_serialization() {
+        let mut map = HashMap::<u8, u8>::new();
+        map.insert(1, 2);
+        map.insert(3, 4);
+
+        let test = no_compression_serialization_test(&map);
+        assert_eq!(test.as_slice(), &[
+            data_ids::MAP_ID,
+            0, 0, 0, 2, // length
+            data_ids::U8_ID, 1, data_ids::U8_ID, 2,
+            data_ids::U8_ID, 3, data_ids::U8_ID, 4,
+        ]);
+    }
+}
