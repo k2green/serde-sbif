929cd59370d614146a23e4d01769878464bf6c67
Kyle Green <kyle.green999@gmail.com>
2023-12-30 15:39:17 +0000

    Update docs

--- src/de.rs
+++ src/de.rs
@@ -54,7 +54,7 @@ impl<R: Read> Deserializer<R> {
     /// fn deserialize_from_bytes<'a, T: serde::Deserialize<'a>>(bytes: &[u8]) -> T {
     ///     let mut cursor = std::io::Cursor::new(bytes);
     ///     let mut deserializer = Deserializer::new(&mut cursor)?;
-    ///     T::deserialize(&mut deserializer)
+    ///     T::deserialize(&mut deserializer).unwrap()
     /// }
     /// ```
     pub fn new(mut reader: R) -> Result<Self, Error> {
